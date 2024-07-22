d04fd3e0640e06992a3c657441f03beba1022ddc
Harish Srinivasan <92076885+Harish2930@users.noreply.github.com>
2024-07-22 14:15:29 +0530

    initial files

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+require('express-async-errors');
+
+const connectDB = require("./db/connect");
+const express = require("express");
+const cors = require('cors')
+const app = express();
+const mainRouter = require("./routes/user");
+
+app.use(express.json());
+
+app.use(cors())
+app.use("/api/v1", mainRouter);
+
+const port = 3000;
+const url = "mongodb+srv://admin:admin2930@cluster0.uoe4quc.mongodb.net/authdb"
+
+const start = async () => {
+
+    try {        
+        await connectDB(url);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        })
+
+    } catch (error) {
+       console.log(error); 
+    }
+}
+
+start();
+
